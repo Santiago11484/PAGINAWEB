4e3ffaf34d50584ded31c496b990f79f9aa81d9e
bruno franca <brunoxx_94@hotmail.com>
2021-10-17 20:31:50 -0300

    add Base de datos

--- PAGINAWEB/Program.cs
+++ PAGINAWEB/Program.cs
@@ -6,13 +6,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace PAGINAWEB
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static string database = "base.db";
+        static async Task Main(string[] args)
         {
+            using (var db = new DatabaseDBContext())
+            {
+                await db.Database.EnsureCreatedAsync();
+
+                var usuario1 = new Usuario()
+                {
+                    Id = 1,
+                    Name = "pepe",
+                    Surname = "papa"
+                };
+                db.Usuarios.Add(usuario1);
+
+                await db.SaveChangesAsync();
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,5 +40,39 @@ namespace PAGINAWEB
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+
+        public class Usuario
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+        }
+
+        public class DatabaseDBContext : DbContext
+        {
+            public DbSet<Usuario> Usuarios { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite(connectionString: "FileName=" + database, sqliteOptionsAction: op =>
+                {
+                    op.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName
+                        );
+                });
+                base.OnConfiguring(optionsBuilder);
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+                modelBuilder.Entity<Usuario>(entity =>
+                {
+                    entity.HasKey(e => e.Id);
+                });
+                base.OnModelCreating(modelBuilder);
+            }
+        }
     }
 }
